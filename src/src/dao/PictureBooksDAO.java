27d3537b8dea3f094ac1754d7fd9f28b3709b033
dojo2023-masaki <masakiamiplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-16 17:15:24 +0900

    ペット図鑑登録DAO

--- src/src/dao/PictureBooksDAO.java
+++ src/src/dao/PictureBooksDAO.java
@@ -75,4 +75,97 @@ public class PictureBooksDAO {
 		// 結果を返す
 				return petsList;
 	}
+
+	// 引数aniaddで指定されたペット図鑑を登録し、成功したらtrueを返す
+	public boolean insert(Pets aniadd) {
+		Connection conn = null;
+		boolean AniaddResult = false;
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/coffee", "milk", "");
+
+			// SQL文を準備する
+			String sql = "insert into PETS values (?, ?, ?, ?, ?, ?. ?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			pStmt.setString(1, null);
+
+			if (aniadd.getUser_id() != null && !aniadd.getUser_id().equals("")) {
+				pStmt.setString(2, aniadd.getUser_id());
+			}
+			else {
+				pStmt.setString(2, null);
+			}
+
+			if (aniadd.getName() != null && !aniadd.getName().equals("")) {
+				pStmt.setString(3, aniadd.getName());
+			}
+			else {
+				pStmt.setString(3, null);
+			}
+
+			if (aniadd.getSex() != null && !aniadd.getSex().equals("")) {
+				pStmt.setString(4, aniadd.getSex());
+			}
+			else {
+				pStmt.setString(4, null);
+			}
+
+			if (aniadd.getBirthday() != null && !aniadd.getBirthday().equals("")) {
+				pStmt.setString(5, aniadd.getBirthday());
+			}
+			else {
+				pStmt.setString(5, null);
+			}
+
+			if (aniadd.getAppeal() != null && !aniadd.getAppeal().equals("")) {
+				pStmt.setString(6, aniadd.getAppeal());
+			}
+			else {
+				pStmt.setString(6, null);
+			}
+
+			if (aniadd.getCry() != null && !aniadd.getCry().equals("")) {
+				pStmt.setString(7, aniadd.getCry());
+			}
+			else {
+				pStmt.setString(7, null);
+			}
+
+			if (aniadd.getPicture() != null && !aniadd.getPicture().equals("")) {
+				pStmt.setString(8, aniadd.getPicture());
+			}
+			else {
+				pStmt.setString(8, null);
+			}
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				AniaddResult = true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		// 結果を返す
+		return AniaddResult;
+	}
+
 }
