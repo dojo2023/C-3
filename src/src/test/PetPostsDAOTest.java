b3a70cdd7b8b0ed1680d0644b6ad075e07a37664
dojo2023-omori <130334772+dojo2023-omori@users.noreply.github.com>
2023-06-21 13:59:22 +0900

    またやらかしかけた サーチテスト途中

--- src/src/test/PetPostsDAOTest.java
+++ src/src/test/PetPostsDAOTest.java
@@ -1,18 +1,13 @@
 package test;
-
 import java.sql.Timestamp;
 import java.util.List;
 
 import dao.PetPostsDAO;
 import model.Pet;
-
 public class PetPostsDAOTest {
-
 	private static final Timestamp CURRENT_TIMESTAMP = null;
-
 	public static void main(String[] args) {
 		PetPostsDAO dao = new PetPostsDAO();
-
 		// select()のテスト
 		System.out.println("---------- select()のテスト ----------");
 		List<Pet> petList2 = dao.select();
@@ -24,13 +19,11 @@ public class PetPostsDAOTest {
 			System.out.println("PICTURE：" + inf.getPicture());
 			System.out.println("TIME：" + inf.getTime());
 		}
-
 		// insert()のテスト
 		System.out.println("---------- insert()のテスト ----------");
 		Pet insRec = new Pet("1", "おうちのお庭でbbq", "1", "#ミニチュアピンシャー", "/img/うみちゃん.png", CURRENT_TIMESTAMP);
 		if (dao.insert(insRec)) {
 			System.out.println("登録成功！");
-
 		}
 			else {
 				System.out.println("登録失敗！");
--- src/src/test/PetSearchDAOTest.java
+++ src/src/test/PetSearchDAOTest.java
@@ -1,19 +1,39 @@
 package test;
 
-import dao.PetSearchDAO;
+import java.util.List;
 
-public class PetSearchDAOTest 
+import dao.PetSearchDAO;
+import model.Pet;
+
+public class PetSearchDAOTest
 {
-	 public static void main(String[] args) 
-	 {
-		 // 検索テスト
-		 searchTest();
-	 }
-	 
-	 private static void searchTest()
-	 {
-		 PetSearchDAO dao = new PetSearchDAO();
-		 //selectのテスト
-		 
-	 }
+    public static void main(String[] args)
+    {
+        // 検索テスト
+        searchTest();
+    }
+
+    private static void searchTest() {
+        PetSearchDAO dao = new PetSearchDAO();
+        System.out.println("---------- select()のテスト ----------");
+        // 検索キーワードを指定
+        String keyWord = "#";
+
+        // selectメソッドを呼び出して検索を実行
+        List<Pet> resultList = dao.select(keyWord);
+
+        // 検索結果の処理
+        if (resultList != null)
+        {
+            for (Pet pet : resultList)
+            {
+                // 取得した検索結果を処理するコードを追加
+                System.out.println(pet);
+            }
+        }
+        else
+        {
+            System.out.println("検索結果がありません。");
+        }
+    }
 }
