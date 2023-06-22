195d2f711c2c260910a504023b463173d121ac54
dojo2023-kita <kitahinakoplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-22 13:29:39 +0900

    メニュー

--- src/src/servlet/MenuServlet.java
+++ src/src/servlet/MenuServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.FavoritesDAO;
 import dao.MenuDAO;
 import model.Pet;
 /**
@@ -51,18 +50,6 @@ public class MenuServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		 doGet(request, response);
-		//リクエストパラメーターを取得、doPostに書く
-		 request.setCharacterEncoding("UTF-8");
-		 String posts = request.getParameter("posts_id");
-		 String fav = request.getParameter("favorite");
-
-		 //いいねが押されたときの処理
-		 FavoritesDAO fDao = new FavoritesDAO ();
-		 //postsを数値に変換する（posts_id)
-		 int posts_id = 1;//postsを変換したもの
-		   //int posts_id = Integer.parseInt(request.getParameter("POSTS_ID"));
-		 //fDaoを利用していいねする updateFavorite(posts_id)
-		 fDao.updateFavorite(posts_id);
 
 	}
 
